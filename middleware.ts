ab09613ddfb3d5e4fe456e402ab523d080257ea6
Izaz Ahamed Mallick <izazahamedmallick98@gmail.com>
2025-01-20 11:46:20 +0530

    middleware task complete

--- middleware.ts
+++ middleware.ts
@@ -1,52 +1,58 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { supabaseServerClient } from "./lib/supabaseClient";
+import { supaBaseInstence } from "./lib/supabaseClient";
 
 export async function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl;
 
     if (pathname.startsWith("/admin")) {
         const token = req.cookies.get("access_token")?.value;
+        console.log("token", token);
         if (!token) {
             console.log("No token found, redirecting to home");
-            return NextResponse.redirect(new URL("/", req.url));
-        }
-        if (!supabaseServerClient) {
-            console.error(
-                "Supabase Service Role Client is not configured properly."
-            );
             return NextResponse.redirect(new URL("/", req.url));
         }
 
         const {
             data: { user },
             error,
-        } = await supabaseServerClient.auth.getUser(token);
-
+        } = await supaBaseInstence.auth.getUser(token);
+        console.log("User ID:", user?.id);
         if (error || !user) {
             console.log("Error getting user from token:", error);
             return NextResponse.redirect(new URL("/", req.url));
         }
 
-        const { data: roles, error: roleError } = await supabaseServerClient
+        const userId = user?.id;
+        if (!userId) {
+            console.log("User ID is missing");
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+
+        const { data: roles, error: roleError } = await supaBaseInstence
             .from("users")
             .select("role")
-            .eq("id", user.id)
-            .single();
+            .eq("id", user?.id)
+            .single(); // Ensures exactly one row is returned
+
+        console.log("Roles Data:", roles);
+        console.log("Role Error:", roleError);
 
         if (roleError || !roles) {
-            console.log("Error fetching user roles:", roleError);
+            console.log("Role not found:", roles);
             return NextResponse.redirect(new URL("/", req.url));
         }
 
         if (roles.role !== "admin") {
             return NextResponse.redirect(new URL("/unauthorized", req.url));
         }
+
+        console.log("User is admin, granting access to admin page");
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/admin/:path*"], // Applies the middleware to any path under /admin
+    matcher: ["/admin/:path*"],
 };
